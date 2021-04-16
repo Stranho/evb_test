bdd35ad7cc96a8a3b9693d7839ae333b1f197616
Stranho <lavdate_dominvm@hotmail.com>
2021-04-16 15:49:29 -0300

    migrate + seed

--- seeds/02_transferencias.js
+++ seeds/02_transferencias.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(() => {
       // Inserts seed entries
       return knex('jogador_transf').insert([
-        {id: 1, jogador_origem: 1, jogador_destino: 2, valor: 3, tipo_operacao: 1, data_operacao: new Date(), operacao_cancelada: false },
-        {id: 2, jogador_origem: 1, jogador_destino: 3, valor: 2, tipo_operacao: 1, data_operacao: new Date(), operacao_cancelada: false },
-        {id: 3, jogador_origem: 3, jogador_destino: 4, valor: 6, tipo_operacao: 1, data_operacao: new Date(), operacao_cancelada: false }
+        {jogador_origem: 1, jogador_destino: 2, valor: 3, tipo_operacao: 1, data_operacao: new Date(), operacao_cancelada: false },
+        {jogador_origem: 1, jogador_destino: 3, valor: 2, tipo_operacao: 1, data_operacao: new Date(), operacao_cancelada: false },
+        {jogador_origem: 3, jogador_destino: 4, valor: 6, tipo_operacao: 1, data_operacao: new Date(), operacao_cancelada: false }
       ]);
     });
 };
