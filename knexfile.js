61b616428d0d6a3a8876fd1f130c5b5e7f9b83bc
Stranho <lavdate_dominvm@hotmail.com>
2021-04-16 17:37:25 -0300

    heroku ssl

--- knexfile.js
+++ knexfile.js
@@ -13,7 +13,10 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: 'postgres://smgxtmbhjxiufs:4cf985dbcc59800b7a59b18d5256bb5c2444d22274557b6f2788fd09d399d6f7@ec2-18-215-111-67.compute-1.amazonaws.com:5432/dar3png1l6apo2?ssl=true',
+    connection: {
+      connectionString: 'postgres://smgxtmbhjxiufs:4cf985dbcc59800b7a59b18d5256bb5c2444d22274557b6f2788fd09d399d6f7@ec2-18-215-111-67.compute-1.amazonaws.com:5432/dar3png1l6apo2',
+      ssl: {rejectUnauthorized: false},
+      }
   }
 
 };
